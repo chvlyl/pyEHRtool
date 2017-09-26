7e25bfbb006e48484096fb1f0a995ee1574808bd
Eric Z Chen <chvlyl@users.noreply.github.com>
2017-09-26 10:26:30 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyEHRtool',
+      version='0.1',
+      description='A python package for processing EHR data',
+      url='http://github.com/chvlyl/pyEHRtool',
+      author='Eric Chen',
+      author_email='chvlyl@gmail.com',
+      license='MIT',
+      packages=['pyEHRtool'],
+      zip_safe=False)
