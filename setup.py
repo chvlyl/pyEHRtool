fa5f8257ddb6174a6ff240ac481efe3f5298a85a
Eric Chen <chvlyl@gmail.com>
2017-09-26 10:40:02 -0400

    init package

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='pyEHRtool',
       version='0.1',
       description='A python package for processing EHR data',
@@ -8,4 +13,13 @@ setup(name='pyEHRtool',
       author_email='chvlyl@gmail.com',
       license='MIT',
       packages=['pyEHRtool'],
+      install_requires=[
+          'pandas',
+          'numpy',
+      ],
+      include_package_data=True,
+      
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      
       zip_safe=False)
